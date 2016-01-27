9903124578886eb90b0f4cd77b09edf2b224dbbd
cmdrtorefresh <cmdrtorefresh@gmail.com>
2016-01-26 22:46:40 -0800

    Buttons positioning done

---
+++ HomeLink/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HomeLink
+//
+//  Created by Stephanie Kirtiadi on 1/26/16.
+//  Copyright © 2016 Dahlia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HomeLink/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HomeLink
+//
+//  Created by Stephanie Kirtiadi on 1/26/16.
+//  Copyright © 2016 Dahlia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
